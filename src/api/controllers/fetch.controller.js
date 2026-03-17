98651b9a41c5ec4eb86a904a29c320801b47011d
Timothy Tew <tewtimothy@gmail.com>
2026-03-17 19:18:12 +1030

    Update fetch.controller.js

--- src/api/controllers/fetch.controller.js
+++ src/api/controllers/fetch.controller.js
@@ -3,11 +3,14 @@ const fetcher = require('../../services/fetcher.service')
 async function fetchFromSources(req, res, next) {
     try {
         const count = await fetcher.fetchFromSources();
-
         return res.status(200).json({
             message: `Fetched ${count} articles.`
         });
     } catch (err) {
+        logger.error('Fetch failed', { 
+            error: err.message,
+            response: err.response?.data  // shows NewsAPI's actual error message
+        });
         next(err);
     }
 }
