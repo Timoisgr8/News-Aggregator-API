595c0297126af57271e2e0379a76fe17d255d161
Timothy Tew <tewtimothy@gmail.com>
2026-03-17 19:32:18 +1030

    Update newsapi.js

--- src/integrations/newsapi.js
+++ src/integrations/newsapi.js
@@ -5,23 +5,15 @@ const logger = require('../utils/logger');
 async function fetchArticles(category) {
     const url = `${config.newsapi.baseUrl}/top-headlines`;
 
-    try {
-        const response = await axios.get(url, {
-            params: {
-                apiKey: config.newsapi.key,
-                category: category || undefined
-            }
-        });
-        return response.data.articles;
-    } catch (err) {
-        logger.error('NewsAPI request failed', {
-            status: err.response?.status,
-            message: err.response?.data?.message,
-            code: err.response?.data?.code,
-            apiKey: config.newsapi.key ? 'set' : 'not set'
-        });
-        throw err;
-    }
+    const response = await axios.get(url, {
+        params: {
+            apiKey: config.newsapi.key,
+            country: 'us',
+            category: category || undefined
+        }
+    });
+
+    return response.data.articles;
 }
 
 module.exports = { fetchArticles };
